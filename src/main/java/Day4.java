01cc24616da47fea54ab524c98d46451f07eed60
Flavian Popa <popaflavian7@gmail.com>
2017-12-06 02:00:05 +0100

    Finish day4

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -1,23 +1,24 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day4 {
 
-    public int getNoOfValidPassphrasesFromFile() throws IOException {
+
+    public int getNoOfValidPasawordsFromFilePartOne() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("src/test/resources/day4input.txt"));
         String firstRow;
         int sum = 0;
         while((firstRow = br.readLine()) != null) {
-            sum += getNoOfValidPassphrases(firstRow);
+            sum += getNoOfValidPasswordsPartOne(firstRow);
         }
         return sum;
     }
 
-    public int getNoOfValidPassphrases(final String input) {
+    public int getNoOfValidPasswordsPartOne(final String input) {
         Map<String, Integer> passMap = new HashMap<>();
         for(String phrase : Arrays.asList(input.split(" "))) {
             if(passMap.containsKey(phrase)) {
@@ -26,5 +27,29 @@ public class Day4 {
             passMap.put(phrase, 0);
         }
         return 1;
+    }
+
+    public int getNoOfValidPasawordsFromFilePartTwo() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/test/resources/day4input.txt"));
+        String firstRow;
+        int sum = 0;
+        while((firstRow = br.readLine()) != null) {
+            sum += getNoOfValidPasswordsPartTwo(firstRow);
+        }
+        return sum;
+    }
+
+    public int getNoOfValidPasswordsPartTwo(final String input) {
+        List<String> words = Arrays.asList(input.split(" "));
+        Set<String> sortedSet = new TreeSet<>();
+        for(String word : words) {
+            String sortedWord = getSortedWord(word);
+            sortedSet.add(sortedWord);
+        }
+        return sortedSet.size() == words.size() ? 1 : 0;
+    }
+
+    public String getSortedWord(String word) {
+        return Stream.of(word.split("")).sorted().collect(Collectors.joining());
     }
 }
--- src/test/java/Day4Test.java
+++ src/test/java/Day4Test.java
@@ -9,14 +9,28 @@ public class Day4Test {
     private Day4 d4 = new Day4();
 
     @Test
-    public void testExamples() {
-        assertEquals(1, d4.getNoOfValidPassphrases("aa bb cc dd ee"));
-        assertEquals(0, d4.getNoOfValidPassphrases("aa bb cc dd aa"));
-        assertEquals(1, d4.getNoOfValidPassphrases("aa bb cc dd aaa"));
+    public void testExamplesPartOne() {
+        assertEquals(1, d4.getNoOfValidPasswordsPartOne("aa bb cc dd ee"));
+        assertEquals(0, d4.getNoOfValidPasswordsPartOne("aa bb cc dd aa"));
+        assertEquals(1, d4.getNoOfValidPasswordsPartOne("aa bb cc dd aaa"));
     }
 
     @Test
-    public void testMyInput() throws IOException {
-        assertEquals(383, d4.getNoOfValidPassphrasesFromFile());
+    public void testMyInputPartOne() throws IOException {
+        assertEquals(383, d4.getNoOfValidPasawordsFromFilePartOne());
+    }
+
+    @Test
+    public void testExamplesPartTwo() throws IOException {
+        assertEquals(1, d4.getNoOfValidPasswordsPartTwo("abcde fghij"));
+        assertEquals(0, d4.getNoOfValidPasswordsPartTwo("abcde xyz ecdab"));
+        assertEquals(1, d4.getNoOfValidPasswordsPartTwo("a ab abc abd abf abj"));
+        assertEquals(1, d4.getNoOfValidPasswordsPartTwo("iiii oiii ooii oooi oooo"));
+        assertEquals(0, d4.getNoOfValidPasswordsPartTwo("oiii ioii iioi iiio"));
+    }
+
+    @Test
+    public void testMyInputPartTwo() throws IOException {
+        assertEquals(265, d4.getNoOfValidPasawordsFromFilePartTwo());
     }
 }
